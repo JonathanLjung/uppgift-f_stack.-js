de8755de39d29a6e4e3389c411e3c03fed8bc241
Jonathan Ljung <jonteljungp@gmail.com>
2022-10-19 01:34:09 +0200

    complete assignment

--- www/dev/README.md
+++ www/dev/README.md
@@ -1,68 +1,79 @@
 ## För att installera och köra appen
 
 ```
-För att starta appen skriver du nedan i terminalen.
+För att starta appen skriver du nedan i terminalen. 
 
-$ npm install
-$ npm start
+ $ npm install
+ $ npm start
 
 (terminalen kan öppnas via kortkommandot ctrl + j)
 ```
 
 ## **Problemformulering**
 
-Jag har valt att göra en hemsida där du kan sortera personer efter förnamn, efternamn eller personlighetstyp (färg). Detta gör du enkelt genom att klicka på kategorien du vill sortera efter.
+Jag har valt att göra en hemsida där du kan sortera personer efter förnamn, efternamn eller personlighetstyp (färg). Detta gör du enkelt genom att klicka på kategorien du vill sortera efter. 
 
-Jag har även gjort en funktion som gör det möjligt att direkt dela in personer i grupper, användaren väljer hur många personer de vill ha i varje grupp och därefter slumpas alla personer och delas in i grupper av antalet som användaren skrivit in. 
+Jag har även lagt in en sök-funktion som gör det möjligt att söka efter en viss kategori, t.ex Förnamn, Efternamn eller Gruppnummer. 
 
-Jag har också gjort så att när man startar hemsidan eller trycker på återställ knappen så ska varje person ha ett gruppnummer var, vilket är indelat via index och inte slumpmässigt. 
+Jag har också gjort en funktion som gör det möjligt att direkt dela in personer i grupper, användaren väljer hur många personer de vill ha i varje grupp och därefter slumpas alla personer och delas in i grupper av antalet som användaren angett.
+
+Jag har också gjort så att när man startar hemsidan eller trycker på återställ knappen så ska varje person ha ett gruppnummer var, vilket är indelat via index och inte slumpmässigt.
 
 ## **Dokumentation**
-Jag började med att göra en HTML fil samt en Javascript fil och länkade ihop dessa med hjälp av script src kommandot. 
+
+Jag började med att göra en HTML fil samt en Javascript fil och länkade ihop dessa med hjälp av script src kommandot.
 
 Sedan hämtade jag data från användare via en github reposition vid namn frontend-1_data. Datan jag hämtade var i json-format, detta la jag sedan in i mappen "data/dev/users".
-Denna data är sparad i arrayer. 
-Jag gjorde sedan en funktion vid namn getUsers, som fetchar informationen jag behöver.  
+Denna data är sparad i arrayer.
+Jag gjorde sedan en funktion vid namn getUsers, som fetchar informationen jag behöver.
 
 #
-Jag skapade sedan en ny div class vid namn container som jag lämnade tom i HTML-filen. Jag gjorde en ny variabel i min JS-fil och döpte den till Container och länkade den med div classen "Container" från HTML-filen. Denna använder jag för att lägga in information på hemsidan på rätt plats. 
 
-Jag gjorde också en ny variabel i min JS-fil vid namn userComponent, där jag la in en div class med all user information jag behöver (förnamn, efternamn, personlighetsfärg & grupp). Jag döpte div classen till userWrapper, varför jag gjorde detta var för att jag senare ska ha det lättare att styla min hemsida. 
+Jag skapade sedan en ny div class vid namn container som jag lämnade tom i HTML-filen. Jag gjorde en ny variabel i min JS-fil och döpte den till Container och länkade den med div classen "Container" från HTML-filen. Denna använder jag för att lägga in information på hemsidan på rätt plats.
+
+Jag gjorde också en ny variabel i min JS-fil vid namn userComponent, där jag la in en div class med all user information jag behöver (förnamn, efternamn, personlighetsfärg & grupp). Jag döpte div classen till userWrapper.
 
 Därefter gjorde jag nya variabler med nya namn med datan jag hämtat från mina json-filer i "dev/data/users". Dessa variabler använder jag senare i olika funktioner.
 
 #
 
-I mina funktioner använder jag en metod kallad addEventListener, detta är en metod som "lyssnar" och agerar, i detta fall lyssnar den på "click", så när jag klickar med musen på knappen jag gjort så kommer funktionen att köras. 
+I mina funktioner använder jag en metod kallad addEventListener, detta är en metod som "lyssnar" och agerar, i detta fall lyssnar den på "click", så när jag klickar med musen på knappen jag gjort så kommer funktionen att köras.
 
-Mina sort funktioner jämför alla namn och delar in de i bokstavsordning. Detta har jag applicerat på förnamn, efternamn och personlighetsfärg. Gruppnummret delas in i nummerordning men det är samma koncept. 
+Mina sort-funktioner jämför alla namn och delar in de i alfabetisk ordning. Detta har jag applicerat på förnamn, efternamn och personlighetsfärg. Gruppnummret delas in i nummerordning men det är samma koncept. Jag har lagt till en If/Else sats som gör att du kan sortera både alfabetiskt samt omvänt.
 
-I varje sort funktion har jag lagt in Container.innerHTML="", detta för att rensa hela innerHTML:en varje gång jag vill sortera på nytt. 
+I varje sort funktion har jag lagt in Container.innerHTML="", detta för att rensa hela innerHTML:en varje gång jag vill sortera på nytt.
 
-Efter varje sort funktion har jag skrivit ut en map metod. Som skriver ut informationen som finns i  userComponent men baserat på sort-funktionen, dvs alfabetisk ordning. 
-
-# 
-
-Jag har gjort en assignGroup funktion som delar in grupperna på index, size & array. Alla personer har en egen index siffra, som börjar på 0. 
-Size är den storlek jag vill att varje grupp ska ha, t.ex har jag satt standarden som 1, så att alla får varsin grupp till en början. Array i denna funktion är alla användare.
-
-I funktionen har jag sedan gjort en ny variabel som heter group, som tar index och lägger till 1, detta för att index börjar på 0 och man vill gärna inte ha gruppnummer 0. 
-Sedan delar jag upp grupperna med hjälp av variabeln size, standard sizen är 1, men jag har gjort en ny variabel vid namn groupAmount som är kopplad till HTML-filen. 
-
-groupAmount är input med typen nummer. Det gör att det kommer upp ett fält där du får skriva in hur många medlemmar du vill ha i varje grupp. 
-
-När du sedan trycker på knappen "Slumpa" så aktiveras en ny funktion vid namn groupBtn, denna funktion sorterar personerna slumpmässigt med hjälp av Math.random, sedan anropas funktionen assignGroup med index-värde 0, groupAmount.value (Det du skrev in i inputen på hemsidan), samt sortedUsers. Detta kommer att tilldela gruppnummer baserat på hur många personer i varje grupp du anger men alla personer har en slumpmässig ordning. Sedan skrivs sortedUsers ut i console log. 
-
-Längst ner anropar jag funktionen getUsers, vilket fetchar all information jag behöver för att koden ska kunna köras. 
+Efter varje sort funktion har jag skrivit ut en map metod. Som skriver ut informationen som finns i userComponent men baserat på sort-funktionen, dvs alfabetisk ordning.
 
 #
+
+Jag har gjort en assignGroup funktion som delar in grupperna på index, size & array. Alla personer har en egen index siffra, som börjar på 0.
+Size är den storlek jag vill att varje grupp ska ha, t.ex har jag satt standarden som 1, så att alla får varsin grupp till en början. Array i denna funktion är alla användare.
+
+I funktionen har jag sedan gjort en ny variabel som heter group, som tar index och lägger till 1, detta för att index börjar på 0 och man vill gärna inte ha gruppnummer 0.
+Sedan delar jag upp grupperna med hjälp av variabeln size, standard sizen är 1, men jag har gjort en ny variabel vid namn groupAmount som är kopplad till HTML-filen.
+
+groupAmount är input med typen nummer. Det gör att det kommer upp ett fält där du får skriva in hur många medlemmar du vill ha i varje grupp.
+
+När du sedan trycker på knappen "Slumpa" så aktiveras en ny funktion vid namn groupBtn, denna funktion sorterar personerna slumpmässigt med hjälp av Math.random, sedan anropas funktionen assignGroup med index-värde 0, groupAmount.value (Det du skrev in i inputen på hemsidan), samt sortedUsers. Detta kommer att tilldela gruppnummer baserat på hur många personer i varje grupp du anger men alla personer har en slumpmässig ordning. Sedan skrivs sortedUsers ut i console log.
+
+Längst ner anropar jag funktionen getUsers, vilket fetchar all information jag behöver för att koden ska kunna köras.
+
+Jag kom till sist på att det kan vara smart att ha en sökfunktion på hemsidan för att lättare kunna lokalisera datan på hemsidan, denna gjorde jag med hjälp av en funktion som jag döpt till searchFunction, som hämtar input från HTML och sedan använder jag en for-loop för att loopa igenom all data i min userWrapper class.
+I min HTML har jag använt mig utav en metod som heter onkeyup, vilket snappar upp så fort jag släpper en tangent. 
+I min funktion har jag en if-sats som bestämmer att om min input inte innehåller data från userWrapper så ska den ta bort "classList", vilket tömmer sidan på all data som inte stämmer överens med datan i userWrapper.
+
+Jag gjorde sedan en else-sats i samma funktion som istället för att ta bort all info så sparar den information som stämmer överens med input. 
+
+På detta vis ser man endast den data man matat in i input. 
+#
+
 **style.css:**
-I min styling till hemsidan har jag tagit inspiration från googles darkmode, jag gick in och inspekterade och hittade rätt färgkombination. Jag har även använt mig av display:flex för att snygga till looken på hemsidan. Jag har också lagt in text-transform:capitalize för att få en bättre helhet, detta för att alla inte hade använt stor bokstav i JSON-filerna. 
+I min styling till hemsidan har jag tagit inspiration från googles darkmode, jag gick in och inspekterade och hittade rätt färgkombination. Jag har även använt mig av display:flex för att snygga till looken på hemsidan. Jag har också lagt in text-transform:capitalize för att få en bättre helhet, detta för att alla inte hade samma använding av stora bokstäver. 
 
+## **Vem har användning av min kod?**
 
+Funktionerna jag har skapat är nödvändiga för alla hemsidor som visar data, man vill kunna sortera och söka för att lätta hitta den data man söker. 
+T.ex en hemsida för en skolklass, då vill du lätt kunna sortera och söka efter de elever du vill ta reda på information om, dessa funktioner kan även användas på online-shopping hemsidor, du vill lätt kunna söka efter den varan du vill köpa för att se t.ex pris, artikelnummer osv. 
 
-
-
-
-
-
+Grupperings-funktionen kan vara bra för b.la lärare för att dela in eleverna i slumpmässiga grupper.
--- www/dev/script.js
+++ www/dev/script.js
@@ -1,96 +1,196 @@
+let firstNameReverse = true; // Skapar nya variabler för att kunna använda sorterings funktionen både fram och bak.
+let lastNameReverse = true;
+let personalityReverse = true;
+let groupReverse = true;
 
-let Container = document.querySelector(".Container") // Gör en ny variabel och kopplar till en class på HTML filen. 
+let Container = document.querySelector(".Container"); // Skapar ny variabel och kopplar till en class på HTML filen.
 
-const userComponent = (user)=>  // Ny div class som importerar data från user.
-` 
-<div class="userWrapper">
+const userComponent = (
+  user // Ny div class som importerar data från user.
+) =>
+  ` 
+<div class="userWrapper flex">
 <span>${user.firstname} </span>
 <span>${user.lastname}</span>
 <span>${user.personalityType}</span>
 <span>${user.group}</span>
 </div>
-`
+`;
 
-function getUsers() { // Funktioner för att fetcha, sortera & dela in i grupper. 
- 
-    fetch('http://localhost:9999/api/users/') // Fetchar information från users (alla användare)
-        .then((response) => response = response.json())
-        .then(users => {
-            const firstNameBtn = document.querySelector("#firstname")
-            const lastNameBtn = document.querySelector("#lastname")
-            const personalityBtn = document.querySelector("#personalityType")
-            const group = document.querySelector("#group")
-
-            Container.innerHTML = users.map(userComponent).join("")
-
-            firstNameBtn.addEventListener("click", ()=>{ // Sorterar förnamn i bokstavsordning.
-                Container.innerHTML=""
-                users.sort(function(a, b){
-                    if(a.firstname < b.firstname) { return -1; }
-                    if(a.firstname > b.firstname) { return 1; }
-                    return 0;
-                })
-                Container.innerHTML = users.map(userComponent).join("")
-
-            })
-            lastNameBtn.addEventListener("click", ()=>{ // Sorterar efternamn i bokstavs-ordning.
-                Container.innerHTML=""
-                users.sort(function(a, b){
-                    if(a.lastname < b.lastname) { return -1; }
-                    if(a.lastname > b.lastname) { return 1; }
-                    return 0;
-                })
-                Container.innerHTML = users.map(userComponent).join("")
-
-            })
-            personalityBtn.addEventListener("click", ()=>{ // Sorterar personlighetsfärg i bokstavs-ordning.
-                Container.innerHTML=""
-                users.sort(function(a, b){
-                    if(a.personalityType < b.personalityType) { return -1; }
-                    if(a.personalityType > b.personalityType) { return 1; }
-                    return 0;
-                })
-                Container.innerHTML = users.map(userComponent).join("")
-
-            })
-        group.addEventListener("click", ()=>{ // Sorterar grupper i nummer-ordning.
-            Container.innerHTML=""
-            users.sort(function(a, b){
-                if(a.group < b.group) { return -1; }
-                if(a.group > b.group) { return 1; }
-                return 0;
-            })
-            Container.innerHTML = users.map(userComponent).join("")
-
-        })
-
-        
-        function assignGroup( index, size, array ) {  // Funktion som delar upp grupper baserat på groupAmount
-        
-            let group = Math.ceil( ( index + 1 ) / size )
-            array[index].group = group
-            index++
-        
-            if ( index >= users.length ) {
-                console.log( "quantity: ", array.length )
-                console.log( "users list: ", array )
-                Container.innerHTML = array.map(userComponent).join("")
-                return true
-            }
-            assignGroup( index, size, array )
-        }
-        let groupBtn = document.querySelector("#groupBtn") // Hämtar ID från HTML-filen.
-        let groupAmount = document.querySelector("#groupAmount") // Hämtar ID från HTML-filen.
-        groupBtn.addEventListener("click", ()=>{
-            const sortedUsers = users.sort(() => Math.random() - 0.5); // Sorterar alla users slumpmässigt med hjälp av Math.random.
-            assignGroup(0,groupAmount.value,sortedUsers)
-            console.log(sortedUsers)
-            
-        })
-        
-        assignGroup(0,1,users) // Standard grupp-indelning, index är 0, 1 är hur många personer det ska vara i varje grupp och users är alla användare. 
-        })
+// Återställnings-knapp
+function resetBtn() {
+  console.log("Hej");
+  window.location.reload();
 }
-getUsers() // Anropar hela funktionen ovan. 
 
+// Sökfunktion
+function searchFunction() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let userWrapper = document.getElementsByClassName("userWrapper");
 
+  for (i = 0; i < userWrapper.length; i++) {
+    if (!userWrapper[i].innerHTML.toLowerCase().includes(input)) {
+      userWrapper[i].classList.remove("flex");
+    } else {
+      userWrapper[i].classList.add("flex");
+    }
+  }
+}
+
+function getUsers() {
+  // Funktioner för att fetcha, sortera & dela in i grupper.
+
+  fetch("http://localhost:9999/api/users/") // Fetchar information från users (alla användare)
+    .then((response) => (response = response.json()))
+    .then((users) => {
+      const firstNameSort = document.querySelector("#firstname");
+      const lastNameSort = document.querySelector("#lastname");
+      const personalitySort = document.querySelector("#personalityType");
+      const groupSort = document.querySelector("#group");
+
+      Container.innerHTML = users.map(userComponent).join("");
+
+      // if else sats som gör så att du kan sortera alfabetiskt samt omvänd alfabetiskt ordning.
+      firstNameSort.addEventListener("click", () => {
+        Container.innerHTML = "";
+        users.sort(function (a, b) {
+          if (firstNameReverse) {
+            if (a.firstname < b.firstname) {
+              return -1;
+            }
+            if (a.firstname > b.firstname) {
+              return 1;
+            }
+            return 0;
+          } else {
+            if (a.firstname < b.firstname) {
+              return 1;
+            }
+            if (a.firstname > b.firstname) {
+              return -1;
+            }
+            return 0;
+          }
+        });
+        Container.innerHTML = users.map(userComponent).join("");
+        firstNameReverse = !firstNameReverse;
+        personalityReverse = true;
+        lastNameReverse = true;
+        groupReverse = true;
+      });
+      // if else sats som gör så att du kan sortera alfabetiskt samt omvänd alfabetiskt ordning.
+      lastNameSort.addEventListener("click", () => {
+        Container.innerHTML = "";
+        users.sort(function (a, b) {
+          if (lastNameReverse) {
+            if (a.lastname < b.lastname) {
+              return -1;
+            }
+            if (a.lastname > b.lastname) {
+              return 1;
+            }
+            return 0;
+          } else {
+            if (a.lastname < b.lastname) {
+              return 1;
+            }
+            if (a.lastname > b.lastname) {
+              return -1;
+            }
+            return 0;
+          }
+        });
+        Container.innerHTML = users.map(userComponent).join("");
+        lastNameReverse = !lastNameReverse;
+        firstNameReverse = true;
+        personalityReverse = true;
+        groupReverse = true;
+      });
+      // if else sats som gör så att du kan sortera alfabetiskt samt omvänd alfabetiskt ordning.
+      personalitySort.addEventListener("click", () => {
+        Container.innerHTML = "";
+        users.sort(function (a, b) {
+          if (personalityReverse) {
+            if (a.personalityType < b.personalityType) {
+              return -1;
+            }
+            if (a.personalityType > b.personalityType) {
+              return 1;
+            }
+            return 0;
+          } else {
+            if (a.personalityType < b.personalityType) {
+              return 1;
+            }
+            if (a.personalityType > b.personalityType) {
+              return -1;
+            }
+            return 0;
+          }
+        });
+        Container.innerHTML = users.map(userComponent).join("");
+        personalityReverse = !personalityReverse;
+        // Återställer alla Reverse variabler till true när du aktiverar funktionen.
+        lastNameReverse = true;
+        groupReverse = true;
+        firstNameReverse = true;
+      });
+      // if else sats som gör så att du kan sortera alfabetiskt samt omvänd alfabetiskt ordning.
+      groupSort.addEventListener("click", () => {
+        Container.innerHTML = "";
+        users.sort(function (a, b) {
+          if (groupReverse) {
+            if (a.group < b.group) {
+              return -1;
+            }
+            if (a.group > b.group) {
+              return 1;
+            }
+            return 0;
+          } else {
+            if (a.group < b.group) {
+              return 1;
+            }
+            if (a.group > b.group) {
+              return -1;
+            }
+            return 0;
+          }
+        });
+        Container.innerHTML = users.map(userComponent).join("");
+        groupReverse = !groupReverse;
+        personalityReverse = true;
+        lastNameReverse = true;
+        firstNameReverse = true;
+      });
+
+      function assignGroup(index, size, array) {
+        // Funktion som delar upp grupper baserat på groupAmount
+
+        let group = Math.ceil((index + 1) / size);
+        array[index].group = group;
+        index++;
+
+        if (index >= users.length) {
+          console.log("quantity: ", array.length);
+          console.log("users list: ", array);
+          Container.innerHTML = array.map(userComponent).join("");
+          return true;
+        }
+        assignGroup(index, size, array);
+      }
+
+      let groupBtn = document.querySelector("#groupBtn"); // Hämtar ID från HTML-filen.
+      let groupAmount = document.querySelector("#groupAmount"); // Hämtar ID från HTML-filen.
+      groupBtn.addEventListener("click", () => {
+        const sortedUsers = users.sort(() => Math.random() - 0.5); // Sorterar alla users slumpmässigt med hjälp av Math.random.
+        assignGroup(0, groupAmount.value, sortedUsers);
+        console.log(sortedUsers);
+      });
+
+      assignGroup(0, 1, users); // Standard grupp-indelning, index är 0, 1 är hur många personer det ska vara i varje grupp och users är alla användare.
+    });
+}
+
+getUsers(); // Anropar hela funktionen ovan.
